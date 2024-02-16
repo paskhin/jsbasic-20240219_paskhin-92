a52296714936dcb26058c08aac16c622e10b9c8f
Ignat Paskhin <paskhin_92@mail.ru>
2024-02-16 15:27:50 +0300

    Находит факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  if (n === 0) {
+    result = 1;
+  } else {
+    for (let i = 1; i < n; i++) {
+      result = result * (n - i);
+    }
+  } return result
 }
