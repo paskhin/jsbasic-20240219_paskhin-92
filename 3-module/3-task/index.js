d760129a55094bc4d96e5b70d0be35b29407e3bc
Ignat Paskhin <paskhin_92@mail.ru>
2024-02-29 12:10:15 +0300

    Преобразует строки

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '-') {
+      arr[i + 1] = arr[i + 1].toUpperCase();
+    }
+  }
+  return arr.filter((item) => item !== '-').join('');
 }
