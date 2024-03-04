3768bace4ad7a62a93a0eb7d790991b1eeca4c38
Ignat Paskhin <paskhin_92@mail.ru>
2024-03-04 22:16:15 +0300

    Создаёт список

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('UL');
+  for (let friend of friends) {
+    ul.insertAdjacentHTML('beforeEnd', `<li>${friend.firstName} ${friend.lastName}</li>`);
+  }
+  return ul
 }
