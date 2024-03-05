b11162c7a6465f56d6344e1772a05b2bc1e88369
Ignat Paskhin <paskhin_92@mail.ru>
2024-03-05 16:51:03 +0300

    Стилизует таблицу

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,18 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    if (table.rows[i].cells[3].dataset.available === 'true') {
+      table.rows[i].classList.add('available');
+    } else if (table.rows[i].cells[3].dataset.available === 'false') {
+      table.rows[i].classList.add('unavailable');
+    } else if (table.rows[i].cells[3].dataset.available === undefined) {
+      table.rows[i].hidden = 'true';
+    } if (table.rows[i].cells[2].innerHTML == 'm') {
+      table.rows[i].classList.add('male');
+    } else if (table.rows[i].cells[2].innerHTML == 'f') {
+      table.rows[i].classList.add('female');
+    } if (table.rows[i].cells[1].innerHTML < 18) {
+      table.rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
+
