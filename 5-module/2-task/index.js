7529c31a8d851846d519b77caab883c1ab52ee88
Ignat Paskhin <paskhin_92@mail.ru>
2024-03-11 17:32:06 +0300

    Переключает видимость текста

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
 function toggleText() {
-  // ваш код...
+  let buttonToggle = document.querySelector('.toggle-text-button');
+  buttonToggle.addEventListener('click', () => {
+    text.hidden === true ? text.hidden = false : text.hidden = true;
+  })
 }
