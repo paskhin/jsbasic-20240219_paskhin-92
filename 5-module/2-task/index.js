9c1517b2b7c589690ae2f2764d5dad41f70a9806
Ignat Paskhin <paskhin_92@mail.ru>
2024-03-12 15:43:24 +0300

    Создаёт слайдер

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,6 +1,6 @@
 function toggleText() {
   let buttonToggle = document.querySelector('.toggle-text-button');
   buttonToggle.addEventListener('click', () => {
-    text.hidden === true ? text.hidden = false : text.hidden = true;
+    text.hidden = !text.hidden;
   })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  let buttonLeft = document.querySelector('.carousel__arrow_left');
+  let buttonRight = document.querySelector('.carousel__arrow_right');
+  let sliderList = document.querySelector('.carousel__inner');
+  let slide = document.querySelector('.carousel__slide');
+  let slides = document.querySelectorAll('.carousel__slide');
+  let slideWidth = slide.offsetWidth;
+  let offSet = 0;
+  let maxOffSet = -slideWidth * (slides.length - 1);
+  let sliderCoord = slide.getBoundingClientRect();
+
+  buttonLeft.style.display = 'none';
+  document.addEventListener('click', function (event) {
+    let target = event.target;
+    let clickCoordLeft = event.clientX - sliderCoord.left;
+    if (!target.closest('.carousel__img') && !target.closest('.carousel__arrow')) {
+      return;
+
+    } else if (clickCoordLeft < slideWidth / 2) {
+      buttonRight.style.display = '';
+
+      if (offSet >= -slideWidth) {
+        buttonLeft.style.display = 'none';
+        offSet = 0;
+      } else {
+        offSet = offSet + slideWidth;
+      }
+
+    } else if (clickCoordLeft > slideWidth / 2) {
+      buttonLeft.style.display = '';
+      offSet = offSet - slideWidth;
+
+      if (offSet <= maxOffSet) {
+        buttonRight.style.display = 'none';
+        offSet = maxOffSet;
+      }
+    }
+    sliderList.style.transform = `translateX(${offSet}px)`;
+  })
 }
